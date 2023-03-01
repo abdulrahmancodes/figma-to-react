2d34d2934233c1b4a8d38b3da28d9a0f7d257a3d
abdulrahmancodes <ar5438376@gmail.com>
2023-03-01 19:52:18 +0530

    add dummy data

---
+++ src/constants/authorsTable.js
@@ -0,0 +1,74 @@
+export const AUTHORS_TABLE = [
+  {
+    author: {
+      name: "Elaine Benes",
+      email: "elaine@vandelay.com",
+    },
+    function: {
+      role: "Manager",
+      subText: "Organization",
+    },
+    isOnline: true,
+    date: "14/06/21",
+  },
+  {
+    author: {
+      name: "Sidra Holland",
+      email: "sidra@vandelay.com",
+    },
+    function: {
+      role: "Programmer",
+      subText: "Developer",
+    },
+    isOnline: false,
+    date: "14/06/21",
+  },
+  {
+    author: {
+      name: "Cosmo Kramer",
+      email: "kramer@vandelay.com",
+    },
+    function: {
+      role: "Executive",
+      subText: "Projects",
+    },
+    isOnline: true,
+    date: "14/06/21",
+  },
+  {
+    author: {
+      name: "Newman",
+      email: "newman@usps.com",
+    },
+    function: {
+      role: "Manager",
+      subText: "Organization",
+    },
+    isOnline: true,
+    date: "14/06/21",
+  },
+  {
+    author: {
+      name: "Frank Costanza",
+      email: "frank@vandelay.com",
+    },
+    function: {
+      role: "Programmer",
+      subText: "Developer",
+    },
+    isOnline: false,
+    date: "14/06/21",
+  },
+  {
+    author: {
+      name: "Art VanDelay",
+      email: "art.ie@vandelay.com",
+    },
+    function: {
+      role: "Designer",
+      subText: "UI/UX Design",
+    },
+    isOnline: false,
+    date: "14/06/21",
+  },
+];
---
+++ src/constants/projects.js
@@ -0,0 +1,32 @@
+export const PROJECTS = [
+  {
+    company: "Software Import/Export",
+    budget: "$14,000",
+    status: "Working",
+    progress: 60,
+  },
+  {
+    company: "Add Progress Track",
+    budget: "$3,000",
+    status: "Canceled",
+    progress: 10,
+  },
+  {
+    company: "Fix Platform Errors",
+    budget: "Not set",
+    status: "Done",
+    progress: 100,
+  },
+  {
+    company: "Launch our Mobile App",
+    budget: "$32,000",
+    status: "Done",
+    progress: 100,
+  },
+  {
+    company: "Add the New Pricing Page",
+    budget: "$400",
+    status: "Working",
+    progress: 25,
+  },
+];
