a674229cf49ea4e461ccbc89c906daadc4053f44
abdulrahmancodes <ar5438376@gmail.com>
2023-03-01 17:20:43 +0530

    add constants

---
+++ src/constants/dashboard.js
@@ -0,0 +1,115 @@
+export const DASHBOARD_FIRST_ROW_ITEMS = [
+  {
+    title: "Today’s Money",
+    value: "$53,000",
+    change: "+55%",
+    hasIncreased: true,
+  },
+  {
+    title: "Today’s Users",
+    value: "2300",
+    change: "+5%",
+    hasIncreased: true,
+  },
+  {
+    title: "New Clients",
+    value: "$53,000",
+    change: "+3,052",
+    hasIncreased: false,
+  },
+  {
+    title: "Total Sales",
+    value: "$173,000",
+    change: "+8%",
+    hasIncreased: true,
+  },
+];
+
+export const USER_STATS = [
+  {
+    text: "Users",
+    value: "32,984",
+    progress: 66,
+  },
+  {
+    text: "Clicks",
+    value: "2,42m",
+    progress: 85.5,
+  },
+  {
+    text: "Sales",
+    value: "2,400$",
+    progress: 40,
+  },
+  {
+    text: "Items",
+    value: "320",
+    progress: 60.5,
+  },
+];
+
+export const PROJECTS = [
+  {
+    name: "Chakra Soft UI Version",
+    members: ["member1", "member2", "member3", "member4", "member5"],
+    budget: "$14,000",
+    progress: 60,
+  },
+  {
+    name: "Add Progress Track",
+    members: ["member1", "member2"],
+    budget: "$3,000",
+    progress: 10,
+  },
+  {
+    name: "Fix Platform Errors",
+    members: ["member1", "member2"],
+    budget: "Not set",
+    progress: 100,
+  },
+  {
+    name: "Launch our Mobile App",
+    members: ["member1", "member2", "member3", "member4"],
+    budget: "$32,000",
+    progress: 100,
+  },
+  {
+    name: "Add the New Pricing Page",
+    members: ["member1", "member2", "member3", "member4", "member5"],
+    budget: "$400",
+    progress: 25,
+  },
+  {
+    name: "Redesign New Online Shop",
+    members: ["member1", "member2"],
+    budget: "$7,600",
+    progress: 40,
+  },
+];
+
+export const ORDERS = [
+  {
+    title: "$2400, Design changes",
+    date: "22 DEC 7:20 PM",
+  },
+  {
+    title: "New order #4219423",
+    date: "21 DEC 11:21 PM",
+  },
+  {
+    title: "Server Payments for April",
+    date: "21 DEC 9:28 PM",
+  },
+  {
+    title: "New card added for order #3210145",
+    date: "20 DEC 3:52 PM",
+  },
+  {
+    title: "Unlock packages for Development",
+    date: "19 DEC 11:35 PM",
+  },
+  {
+    title: "New order #9851258",
+    date: "18 DEC 4:41 PM",
+  },
+];
